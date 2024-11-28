fc0b0c0f214b2c0fd3a5036db411f2f77675369a
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-11-28 09:20:53 -0300

    Estilizando o Focus no Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,13 +11,12 @@ export function Input({ ...props }: InputProps) {
       px='$4'
       borderWidth='$0'
       borderRadius='$md'
+      $focus={{
+        borderWidth: '$1',
+        borderColor: '$green500',
+      }}
     >
-      <InputField
-        color='$white'
-        fontFamily='$body'
-        placeholderTextColor='$gray300'
-        {...props}
-      />
+      <InputField color='$white' placeholderTextColor='$gray300' {...props} />
     </GluestackInput>
   );
 }
