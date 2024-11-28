93d0f4cbf482235d420bbaa68483dfbcfc7d05b0
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-11-28 08:30:32 -0300

    Utilizando SVG

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import {
 import { Loading } from '@components/Loading';
 import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { SignIn } from '@screens/SignIn';
+import { StatusBar } from 'react-native';
 import { config } from './config/gluestack-ui.config';
 
 export default function App() {
@@ -15,8 +16,17 @@ export default function App() {
     Roboto_700Bold,
   });
 
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <GluestackUIProvider config={config}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
       {fontsLoaded ? <SignIn /> : <Loading />}
     </GluestackUIProvider>
   );
