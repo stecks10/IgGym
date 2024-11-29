e2812449117af4709d86edf4bbb80205e06d19c8
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-11-29 00:08:44 -0300

    Componente UserPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,22 @@
 import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed';
+import { UserPhoto } from './UserPhoto';
 
 export function HomeHeader() {
   return (
-    <HStack bg='$gray600' pt='$16' pb='$5' px={'$8'} alignItems='center'>
+    <HStack
+      bg='$gray600'
+      pt='$16'
+      pb='$5'
+      px={'$8'}
+      alignItems='center'
+      gap={'$4'}
+    >
+      <UserPhoto
+        source={{ uri: 'https://github.com/stecks10.png' }}
+        alt='Photo do usuário'
+        w={'$16'}
+        h={'$16'}
+      />
       <VStack>
         <Text color='$white' fontSize={'$sm'}>
           Olá,
