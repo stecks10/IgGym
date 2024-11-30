f20ffbc21934dbef5bd5ac4ccc8e6e94c4f6cffa
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-11-30 11:06:13 -0300

    Foto Do Usuário

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,21 @@
 import { ScreenHeader } from '@components/ScreenHeader';
-import { VStack } from '@gluestack-ui/themed';
+import { UserPhoto } from '@components/UserPhoto';
+import { Center, VStack } from '@gluestack-ui/themed';
+import { ScrollView } from 'react-native';
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title='Perfil' />
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt={'$6'} px={'$10'}>
+          <UserPhoto
+            source={{ uri: 'https://github.com/stecks10.png' }}
+            alt='Foto do usuário'
+            size='xl'
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   );
 }
