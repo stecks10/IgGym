2082ff0c455003339441d26a461dd437a9c83b2e
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-11-30 10:15:28 -0300

    Componente HistoryCard

---
+++ src/components/HistoryCard.tsx
@@ -0,0 +1,33 @@
+import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed';
+
+export function HistoryCard() {
+  return (
+    <HStack
+      w='$full'
+      px={'$4'}
+      py={'$4'}
+      mb={'$3'}
+      bg='$gray600'
+      rounded={'$md'}
+      alignItems='center'
+      justifyContent='space-between'
+    >
+      <VStack mr={'$5'}>
+        <Heading
+          fontSize={'$md'}
+          fontFamily='$heading'
+          color='$white'
+          textTransform='capitalize'
+        >
+          Costas
+        </Heading>
+        <Text color='$gray100' fontSize={'$lg'} numberOfLines={1}>
+          Puxada frontal
+        </Text>
+      </VStack>
+      <Text color='$gray300' fontSize={'$md'}>
+        08:00
+      </Text>
+    </HStack>
+  );
+}
