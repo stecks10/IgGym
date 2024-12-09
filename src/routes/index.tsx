5a9d4e01f32cc76898a0bbf4df4376aaf569180f
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-12-09 19:04:41 -0300

    Criando seu próprio Hook

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,10 +5,12 @@ import { Box } from '@gluestack-ui/themed';
 import { gluestackUIConfig } from '../../config/gluestack-ui.config';
 import { AuthRoutes } from './auth.routes';
 
+import { useAuth } from '@hooks/useAuth';
 import { useContext } from 'react';
 
 export function Routes() {
   const contextData = useContext(AuthContext);
+  const { user } = useAuth();
 
   console.log(contextData);
 
