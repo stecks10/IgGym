50bb6915227e8d4eec006cb385ba3b77e9ddacc1
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-12-10 19:05:34 -0300

    Criando conta e logando usuario

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -18,7 +18,9 @@ import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 import { ToastMessage } from '@components/ToastMessage';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useAuth } from '@hooks/useAuth';
 import { AppError } from '@utils/AppError';
+import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { api } from '../service/api';
@@ -44,6 +46,8 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
+  const { singIn } = useAuth();
   const toast = useToast();
   const {
     control,
@@ -61,8 +65,12 @@ export function SignUp() {
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
     try {
-      const response = await api.post('/users', { name, email, password });
+      setIsLoading(true);
+      await api.post('/users', { name, email, password });
+
+      await singIn(email, password);
     } catch (error) {
+      setIsLoading(false);
       const isAppError = error instanceof AppError;
       const title = isAppError
         ? error.message
@@ -141,6 +149,7 @@ export function SignUp() {
                   onChangeText={onChange}
                   value={value}
                   errorMessage={errors.password?.message}
+                  textContentType='oneTimeCode'
                 />
               )}
             />
@@ -157,6 +166,7 @@ export function SignUp() {
                   onSubmitEditing={handleSubmit(handleSignUp)}
                   returnKeyType='send'
                   errorMessage={errors.password_confirm?.message}
+                  textContentType='oneTimeCode'
                 />
               )}
             />
@@ -164,6 +174,7 @@ export function SignUp() {
             <Button
               title='Criar e acessar'
               onPress={handleSubmit(handleSignUp)}
+              isLoading={isLoading}
             />
           </Center>
 
