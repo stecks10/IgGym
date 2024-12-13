ba3bb56cb8fd02f7a63dd2ee87e00c88eb71e6ba
Vitor Nunes do Nascimento <stecks10@gmail.com>
2024-12-13 10:21:50 -0300

    Incluindo gerenciador de Token na API

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,19 +1,31 @@
 import { AppError } from '@utils/AppError';
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
+
+type SignOut = () => void;
+
+type ApiInstanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void;
+};
 
 const api = axios.create({
   baseURL: 'http://192.168.1.72:3333',
-});
+}) as ApiInstanceProps;
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.data) {
-      return Promise.reject(new AppError(error.response.data.message));
-    } else {
-      return Promise.reject(error);
+api.registerInterceptTokenManager = (signOut) => {
+  const interceptTokenManager = api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response && error.response.data) {
+        return Promise.reject(new AppError(error.response.data.message));
+      } else {
+        return Promise.reject(error);
+      }
     }
-  }
-);
+  );
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager);
+  };
+};
 
 export { api };
